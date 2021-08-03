f43df5d00636a3b434d240022612e45894b69aab
Alexey Slyusarenko <slyusarenko.a.n@gmail.com>
2021-08-03 08:23:50 +0300

    Add nav buttons, control buttons, navigate menu and control lists mocks

---
+++ src/client/js/mocks/button-control.js
@@ -0,0 +1,20 @@
+const controlButton = [
+  {
+    id: '101',
+    title: 'light',
+    icon: 1001,
+    type: 'slide',
+    value: 25,
+  },
+  {
+    id: '102',
+    title: 'electric',
+    icon: 1002,
+    type: 'switch',
+    value: 'on',
+  },
+];
+
+export {
+  controlButton,
+}
---
+++ src/client/js/mocks/nav.js
@@ -0,0 +1,30 @@
+const nav = {
+  quick: {
+    menu: [
+      {
+        title: 'quick',
+        buttons: [1001, 1002, 1003, 1004, 1005],
+      }
+    ],
+  },
+  main: {
+    menu: [
+      {
+        title: '2st',
+        buttons: [1001, 1002, 1001, 1002],
+      },
+      {
+        title: '2st',
+        buttons: [1001, 1002, 1001, 1003],
+      },
+      {
+        title: '2st',
+        buttons: [1001, 1002, 1001, 1004],
+      },
+    ],
+  },
+};
+
+export {
+  nav,
+}
